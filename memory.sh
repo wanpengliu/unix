b12d86afac08a69a28d1ea6ad1fcb455072fda92
wanpengliu <wallaceking0@hotmail.com>
2016-11-08 15:16:11 +0000

    message

--- memory.sh
+++ memory.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-echo "Hello Meomory"
-
+touch history.txt
 while (true)
 do
-	echo "current time"  $(date +'%Y-%m-%d %H:%M:%S')
+	 CUR_TIME=$(date +'%Y-%m-%d %H:%M:%S')
+	 CUR_MEM=$(sar -r | grep -i average | awk '{print $4}')
+	 echo $CUR_TIME $CUR_MEM >> history.txt
 	sleep 5
 done
